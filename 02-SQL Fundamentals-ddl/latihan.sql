3cff9fb26db8deabe466b0d1aa74028bd55475b6
Handika Saputra <handikasaputra484@gmail.com>
2026-09-09 19:09:03 +0700

    SQL Fundamentals & DDL (Data Definition)

--- 02-SQL Fundamentals-ddl/latihan.sql
+++ 02-SQL Fundamentals-ddl/latihan.sql
@@ -1,44 +1,23 @@
-CREATE TABLE Mahasiswa(
-    id INT,
-    nama VARCHAR(100),
-    jurusan VARCHAR(100)
-);
-
-USE data_mahasiswa;
-SELECT * FROM mahasiswa;
-DESCRIBE mahasiswa;
-SHOW TABLES;
-ALTER TABLE mahasiswa
-ADD passwoard VARCHAR(50) AFTER jurusan;
-ALTER TABLE mahasiswa
-ADD Uvinersitas VARCHAR(50) AFTER jurusan;
-DROP TABLE mahasiswa;
-INSERT INTO mahasiswa(nama, jurusan, alamat, email)
-VALUES ('Handika Saputra', 'Pendidikan Agama Islam', 'Kota Tangerang','handikasaputra484@gmail.com')
-
-DELETE FROM mahasiswa WHERE id =1;
-
--- Buat Tabel users
-CREATE TABLE users (
+CREATE DATABASE belajar;
+USE belajar;
+SELECT * FROM users;
+SELECT * FROM products;
+-- Membuat tabel users
+CREATE TABLE users(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    full_name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL,
-    phone VARCHAR(20),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    nama VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    waktu TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 -- Buat Tabel products
-CREATE TABLE products (
+CREATE TABLE products(
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(150) NOT NULL,
-    description TEXT,
-    price DECIMAL(10, 2) NOT NULL,
-    stock INT DEFAULT 0,
-    category VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    nama VARCHAR(100) NOT null,
+    deskripsi text,
+    harga DECIMAL(10, 2) NOT NULL,
+    Stok INT DEFAULT 0
 );
-
 -- Kalau tabel sudah ada juga dan mau mulai dari nol, hapus dulu:
 DROP TABLE IF EXISTS users, products;
 Lalu jalankan ulang query CREATE TABLE di atas.
