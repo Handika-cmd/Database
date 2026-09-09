b5c52650161ef5a0f5895ff2c8ef096c12d47681
Handika Saputra <handikasaputra484@gmail.com>
2026-09-10 00:54:23 +0700

    SQL CRUD & MDL (Data Manipulation)

---
+++ 04-SQL CRUD & MDL (Data Manipulation)/README.md
@@ -0,0 +1,6 @@
+# target hari ini adalah: Filter produk berdasarkan kategori & harga.
+
+kemarin kita belajar bagaimana memasukkan dan melihat semua data, hari ini kita belajar cara mencari dan mengurutkan data spesifik. Bayangkan aja kalau di database ada 1 juta data, kamu pasti nggak mau lihat semuanya sekaligus kan? Makanya WHERE, LIKE, ORDER BY, dan LIMIT ini sangat penting.
+
+**Tapi sebelumnya,** tabel products kita kemarin belum punya kolom category (kategori). Jadi kita tambahin dulu kolomnya dan masukin beberapa data baru biar seru.
+
---
+++ 04-SQL CRUD & MDL (Data Manipulation)/latihan.sql
@@ -0,0 +1,17 @@
+USE belajar;
+
+SELECT * FROM products;
+
+-- tambahin column karna kita kurang kolom kategori (kategori)
+ALTER TABLE products 
+    CHANGE COLUMN Harga DECIMAL(10, 2) NOT NULL;
+
+INSERT INTO products (Nama, Deskripsi, Harga, Stock, Kategori) VALUES
+('Laptop Lenovo Slim 3', 'Laptop Entry level', 10000000.00, 25, 'Elektronik'),
+('Mouse Logitech G502', 'Mouse eksternal', 250000, 15, 'Aksesoris'),
+('Keyboard Mechanical', 'Keyboard eksternal yang nyaman', 750000, 10, 'Aksesoris'),
+('Monitor LG 24 inch', 'Monitor oled 224Hz', 1800000, 5, 'Elektronik'),
+('Kopi Robusta 1kg', 'Kopi segar dan hangat siap disajikan', 120000, 50, 'Konsumsi');
+
+DELETE FROM products
+    WHERE id = 5;
